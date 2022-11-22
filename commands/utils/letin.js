aa5fc41cf02fa67f9ea3601eb23e8ab13a60ee21
Michael <huangmichael150@gmail.com>
2022-11-22 02:41:00 -0600

    update letin command to handle errors

--- commands/utils/letin.js
+++ commands/utils/letin.js
@@ -15,15 +15,17 @@ module.exports = {
     execute({ inter }) {
         const user = inter.options.getUser('user');
         const member = inter.guild.members.cache.get(user.id);
-
         const champs = inter.guild.roles.cache.find(role => role.name === 'Champs');
-        const active = inter.guild.roles.cache.find(role => role.name === 'Active');
 
         if (member.bot) return inter.reply({ content: `You can't let a bot in ${inter.member}... try again ? ❌`, ephemeral: true });
 
         if (member.roles.cache.has(champs.id)) return inter.reply({ content: `This user is already a champ ${inter.member}... try again ? ❌`, ephemeral: true });
 
-        member.roles.add(champs);
-        return inter.reply({ content: `${inter.user} has let ${user} in! ✅` });
+        member.roles.add(champs)
+            .then(() => inter.reply({ content: `${inter.user} has let ${user} in! ✅` }))
+            .catch((e) => {
+                console.log(e);
+                inter.reply({ content: `Something went wrong ${inter.member}... try again ? ❌`, ephemeral: true })}
+            );
     },
 };
